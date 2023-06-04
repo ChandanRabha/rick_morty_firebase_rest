eafc36d6510832dc17dac84990673995be7b736b
Chandan <chandan@wecreate.app>
2023-06-04 14:21:53 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
-import './App.css'
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+
 import IResponseModel, { Result } from './models/IResponseModel';
 
-function App() {
+import './App.css'
+
+const App = memo(() => {
   const [data, setData] = useState<Result[]>();
+
+  const [hoveredImage, setHoveredImage] = useState("");
 
   const fetchData = useCallback(async () => {
     console.log("Loading");
@@ -28,9 +32,23 @@ function App() {
     fetchData();
   }, [fetchData]);
 
-  useEffect(() => {
-    data?.length && console.log(data);
-  }, [data])
+  const charactersJsx = useMemo(() => data?.map(eachData =>
+    <div className='drop-shadow-xl gap-2 flex flex-col' key={eachData.id}>
+      <img
+        src={eachData.image}
+        alt={eachData.name}
+        className={`rounded-md transition-opacity duration-300 ${eachData.id.toString() !== hoveredImage && hoveredImage !== "" && "opacity-70"}`}
+        id={eachData.id.toString()}
+        onMouseOver={(e) => setHoveredImage(e.target["id" as keyof typeof e.target] as unknown as string)}
+        onMouseLeave={() => setHoveredImage("")}
+      />
+      <div className='flex flex-col dark:text-white drop-shadow-xl'>
+        <label>{eachData.name}</label>
+        <label>{eachData.species}</label>
+        <label>{eachData.status}</label>
+      </div>
+    </div>), [data, hoveredImage])
+
 
   return (
     <div className='flex flex-col md:flex-row gap-5 p-5 sm:p-10 relative dark:bg-mainBG bg-cover bg-no-repeat bg-fixed'>
@@ -41,19 +59,11 @@ function App() {
         <label className='dark:text-white'>CHARACTERS</label>
       </div>
       <div className="flex flex-wrap justify-evenly gap-5 ">
-        {data?.map(eachData =>
-          <div className='drop-shadow-xl gap-2 flex flex-col'>
-            <img src={eachData.image} alt={eachData.name} className='rounded-md' />
-            <div className='flex flex-col dark:text-white drop-shadow-xl'>
-              <label>{eachData.name}</label>
-              <label>{eachData.species}</label>
-              <label>{eachData.status}</label>
-            </div>
-          </div>)}
+        {charactersJsx}
       </div>
     </div>
 
   )
-}
+})
 
 export default App
